adce4050749bcbc739138e2ec38f374c3e9c79e8
Steven Elliott <steven.elliott@live.com>
2021-03-28 19:48:00 -0400

    homework redo - addings prompts

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+var genBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
 // creating arrays of the allowed characters
 
-var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numbArray = ["0","1","2","3","4","5","6","7","8","9"];
-var characterArray = ["@","#","$","%","^","&","*","(",")","-","_","=","+","<",">","?"];
+var uppercaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowercaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numbersArr = ["0","1","2","3","4","5","6","7","8","9"];
+var symbolsArr = ["@","#","$","%","^","&","*","(",")","-","_","=","+","<",">","?"];
+
+// create empty arrays
+
+var resultArray = [];
+
+uppercaseArr [1]
+
+var promptCharacters = prompt ("How many characters do you want? (Between 8 and 128)");
+var promptNumbers = confirm ("Do you want numbers in your password?");
+var promptUpper = confirm ("Do you want UPPERCASE letters in your password?");
+var promptLower = confirm ("Do you want lowercase letters in your password?");
+var promptSymbols = confirm ("Do you want symbols in your password?");
+
+
+// get the conditions of array
+
